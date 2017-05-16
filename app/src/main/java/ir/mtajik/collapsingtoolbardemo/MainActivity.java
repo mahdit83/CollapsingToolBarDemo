2ae05bfe3da633d18787c7dff58d42ad9aa0f7b6
mahdi-tajik <mahdi.tajik@gmail.com>
2017-05-16 18:28:44 +0430

    jenkins test

--- app/src/main/java/ir/mtajik/collapsingtoolbardemo/MainActivity.java
+++ app/src/main/java/ir/mtajik/collapsingtoolbardemo/MainActivity.java
@@ -26,17 +26,7 @@ public class MainActivity extends AppCompatActivity {
         toolbar.inflateMenu(R.menu.menu_main);
         toolbar.setTitle("www.mahditajik.ir");
 
-
-//        setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        System.out.println("jenkins test");
 
         fillData();
     }
