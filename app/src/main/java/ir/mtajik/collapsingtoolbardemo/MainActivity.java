6aa4483784a76155dfd01e3fcd6baab7869ddff5
mahdi-tajik <mahdi.tajik@gmail.com>
2017-05-17 10:05:20 +0430

    jenkins test2

--- app/src/main/java/ir/mtajik/collapsingtoolbardemo/MainActivity.java
+++ app/src/main/java/ir/mtajik/collapsingtoolbardemo/MainActivity.java
@@ -27,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle("www.mahditajik.ir");
 
         System.out.println("jenkins test");
+        sdafdfa
+
+                dfs
 
         fillData();
     }
